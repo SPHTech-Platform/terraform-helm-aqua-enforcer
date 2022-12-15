2d3d46f5cd5565392e56f4c848a03c831cf70a47
Uchinda Padmaperuma <89894943+uchinda-sph@users.noreply.github.com>
2022-12-15 11:13:38 +0800

    test to default enforcers

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "aquasec_enforcer_groups" "agent" {
-  group_id = "bt-uat-agent"
+  group_id = "default"
 }
 
 data "aquasec_enforcer_groups" "kube_enforcer" {
-  group_id = "bt-uat-kube-enforcer"
+  group_id = "default kube enforcer group"
 }
